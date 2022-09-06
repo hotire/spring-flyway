53e9a947b07d9b63efb3d3f472f3368d27301233
hotire <gngh0101@gmail.com>
2022-09-07 00:29:02 +0900

    Init

---
+++ src/test/kotlin/com/github/hotire/springflyway/SpringFlywayApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.github.hotire.springflyway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringFlywayApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}

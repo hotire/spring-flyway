3fcdcceeb76768e492737b6b3c48312e57bdfff8
hotire <gngh0101@gmail.com>
2022-09-08 00:18:26 +0900

    Simplify

--- src/main/kotlin/com/github/hotire/springflyway/Application.kt
+++ src/main/kotlin/com/github/hotire/springflyway/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SpringFlywayApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<SpringFlywayApplication>(*args)
+    runApplication<Application>(*args)
 }
